74ce472bda5ac04cc6b5964ca61e2a5860aed3bd
Minerva <minerva.shrivastava@gmail.com>
2017-12-15 00:04:23 +0530

    Changes in sychronization

--- Practice/src/Thread/Synchronization/Main.java
+++ Practice/src/Thread/Synchronization/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		public static void main(String[] args) {
 			Account a = new Account(99,"minu",9000);
 			
+			//Two Threads
 			Bank b = new Bank(a);
 			ATM a1 = new ATM(a);
 			
